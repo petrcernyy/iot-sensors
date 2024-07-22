05a18dc291bbecb318758ce8e23a31381c0a7e83
Petr Černý <98986671+petrcernyy@users.noreply.github.com>
2024-07-22 21:39:01 +0200

    Update
    Added mobile support

--- nodejs_web_app/app.js
+++ nodejs_web_app/app.js
@@ -14,66 +14,60 @@ res.sendFile(__dirname + '/views/index.html');
 });
 
 
-io.on('connection', function(socket) {
 
-    const mqtt = require('mqtt');
+const mqtt = require('mqtt');
 
-    const protocol = 'mqtt'
-    const host = '192.168.0.104'
-    const port = '1883'
-    const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+const protocol = 'mqtt'
+const host = '192.168.1.240'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 
-    const connectUrl = `${protocol}://${host}:${port}`
+const connectUrl = `${protocol}://${host}:${port}`
 
-    const topic = '/home/temperature'
-    const topic2 = '/home/humidity'
-    const topic3 = '/home/led'
+const topic = '/home/temperature'
+const topic2 = '/home/humidity'
+const topic3 = '/home/led'
 
-    const client = mqtt.connect(connectUrl, {
-        clientId,
-        clean: true,
-        connectTimeout: 4000,
-        username: 'emqx',
-        password: 'public',
-        reconnectPeriod: 1000,
+let temperature;
+let humidity;
+
+const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: 'emqx',
+    password: 'public',
+    reconnectPeriod: 1000,
+})
+
+client.on('connect', () => {
+    console.log('Connected')
+    client.subscribe([topic], () => {
+        console.log(`Subscribe to topic '${topic}'`)
     })
-    
-    client.on('connect', () => {
-        console.log('Connected')
-        client.subscribe([topic], () => {
-            console.log(`Subscribe to topic '${topic}'`)
+    client.subscribe([topic2], () => {
+        console.log(`Subscribe to topic '${topic2}'`)
+    })
+
+    io.on('connection', function(socket) {
+
+        client.on('message', (topic, payload) => {
+            console.log('Received Message:', topic, payload.toString())
+            if (topic.localeCompare("/home/temperature") == 0) {
+                temperature = payload.toString();
+                socket.emit('temperature', payload.toString());
+            }
+            else if (topic.localeCompare("/home/humidity") == 0) {
+                humidity = payload.toString();
+                socket.emit('humidity', payload.toString());
+            }
         })
-        client.subscribe([topic2], () => {
-            console.log(`Subscribe to topic '${topic2}'`)
+
+        socket.on('getData', function() {
+            socket.emit('temperature', temperature);
+            socket.emit('humidity', humidity);
         })
     })
-
-    client.on('message', (topic, payload) => {
-        console.log('Received Message:', topic, payload.toString())
-        if (topic.localeCompare("/home/temperature") == 0) {
-            socket.emit('temperature', payload.toString());
-        }
-        else if (topic.localeCompare("/home/humidity") == 0) {
-            socket.emit('humidity', payload.toString());
-        }
-    })
-
-    socket.on('led', function(data) {
-        if (data) {
-            client.publish(topic3, 'on', { qos: 0, retain: false }, (error) => {
-                if (error) {
-                console.error(error)
-                }
-            })
-        } else {
-            client.publish(topic3, 'off', { qos: 0, retain: false }, (error) => {
-                if (error) {
-                console.error(error)
-                }
-            })
-        }
-    })
-
 })
 
 app.get('/', (req, res) => {
