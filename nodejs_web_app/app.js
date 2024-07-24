14b1def67143f8b218e5c91764a5757edf383995
Petr Černý <98986671+petrcernyy@users.noreply.github.com>
2024-07-24 21:32:46 +0200

    Update

--- nodejs_web_app/app.js
+++ nodejs_web_app/app.js
@@ -13,23 +13,14 @@ app.get('/', (req, res) => {
 res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
 const mqtt = require('mqtt');
 
 const protocol = 'mqtt'
-const host = '192.168.1.240'
+const host = '192.168.0.104'
 const port = '1883'
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 
 const connectUrl = `${protocol}://${host}:${port}`
-
-const topic = '/home/temperature'
-const topic2 = '/home/humidity'
-const topic3 = '/home/led'
-
-let temperature;
-let humidity;
 
 const client = mqtt.connect(connectUrl, {
     clientId,
@@ -40,32 +31,54 @@ const client = mqtt.connect(connectUrl, {
     reconnectPeriod: 1000,
 })
 
+let scenes = [];
+
 client.on('connect', () => {
+
     console.log('Connected')
-    client.subscribe([topic], () => {
-        console.log(`Subscribe to topic '${topic}'`)
-    })
-    client.subscribe([topic2], () => {
-        console.log(`Subscribe to topic '${topic2}'`)
+    client.subscribe(['/ready'], () => {
+        console.log(`Subscribe to topic /ready`)
     })
 
     io.on('connection', function(socket) {
 
         client.on('message', (topic, payload) => {
             console.log('Received Message:', topic, payload.toString())
-            if (topic.localeCompare("/home/temperature") == 0) {
-                temperature = payload.toString();
-                socket.emit('temperature', payload.toString());
+
+            let mess = topic.split('/');
+
+            for (let i = 0; i < scenes.length; i++) {
+                if (mess[1].localeCompare(scenes[i].name) == 0) {
+                    if (mess[2].localeCompare("humidity") == 0) {
+                        scenes[i].humidity = payload.toString();
+                    } else if (mess[2].localeCompare("temperature") == 0) {
+                        scenes[i].temperature = payload.toString();
+                    }
+                    socket.emit('data/ready');
+                }
             }
-            else if (topic.localeCompare("/home/humidity") == 0) {
-                humidity = payload.toString();
-                socket.emit('humidity', payload.toString());
+            
+            if (topic.localeCompare("/ready") == 0) {
+                socket.emit('/ready');
             }
         })
 
-        socket.on('getData', function() {
-            socket.emit('temperature', temperature);
-            socket.emit('humidity', humidity);
+        socket.on('data/getData', function() {
+            socket.emit('data/returnData', scenes);
+        })
+
+        socket.on('data/getDataInit', function() {
+            socket.emit('data/returnDataInit', scenes);
+        })
+
+        socket.on('scenes/saveScene', function(scene) {
+            scenes.push(scene);
+            client.subscribe([`/${scene.name}/temperature`], () => {
+                console.log(`Subscribe to topic /${scene.name}/temperature`);
+            })
+            client.subscribe([`/${scene.name}/humidity`], () => {
+                console.log(`Subscribe to topic /${scene.name}/humidity`);
+            })
         })
     })
 })
@@ -75,5 +88,5 @@ app.get('/', (req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log('listening on port 3000');
 });
